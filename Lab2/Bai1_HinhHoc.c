e9d24e00f1a26ba644986b7ec7553be125e0a6bf
haonhut891-pixel <haonhut891@gmail.com>
2026-07-21 21:41:59 +0700

    Hoan Thanh lab2

---
+++ Lab2/Bai1_HinhHoc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define PI 3.14159;
+const float PII = 3.14159;
+int main(){
+    //Nhap dai, rong tinh chu vi, dien tich hcn
+    float dai, rong, chuViHCN, dienTichHCN, r, chuViHT, dienTichHT;
+    printf("Nhap chieu dai:\nNhap chieu rong:\n");
+    scanf("%f%f", &dai,&rong);
+    chuViHCN = (dai+rong)*2;
+    dienTichHCN = dai*rong;
+    printf("Hinh chu nhat\nDai: %2.f\nRong: %2.f\nChu vi: %2.f\nDien tich: %2.f",dai,rong,chuViHCN,dienTichHCN);
+
+    printf("\nNhap ban kinh: ");
+    scanf("%f", &r);
+    chuViHT = r*2*PI;    //duong kinh * PI
+    dienTichHT = r*r*PII;
+    printf("Hinh tron\nBan kinh: %F\nChu vi: %f\nDien tich: %f",r,chuViHT,dienTichHT);
+    return 0;
+}
